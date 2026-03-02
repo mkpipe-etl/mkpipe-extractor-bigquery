4e8d541e4c81d087d6422d3347721b20d3b346b6
m-karakus <s.metinkarakus@gmail.com>
2026-03-03 00:05:20 +0300

    initial setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# mkpipe-extractor-bigquery
+
+Google BigQuery extractor plugin for mkpipe. Uses spark-bigquery-connector.
+
+Requires `spark-bigquery-connector` JAR in Spark classpath.
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mkpipe-extractor-bigquery',
+    version='1.0.0',
+    license='Apache License 2.0',
+    packages=find_packages(),
+    install_requires=['mkpipe'],
+    include_package_data=True,
+    entry_points={
+        'mkpipe.extractors': [
+            'bigquery = mkpipe_extractor_bigquery:BigQueryExtractor',
+        ],
+    },
+    description='Google BigQuery extractor for mkpipe.',
+    author='Metin Karakus',
+    author_email='metin_karakus@yahoo.com',
+    python_requires='>=3.9',
+)
